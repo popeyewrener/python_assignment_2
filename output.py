1253c557cf37a23e769d77ce56d0d953ddafacdf
popeyewrener <72228782+popeyewrener@users.noreply.github.com>
2022-01-17 20:28:52 +0530

    output update

---
+++ output.py
@@ -0,0 +1,98 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:\Users\Ranjeeta\Music\py_assign\py2\allquestion.py ========
+Program by CHAITANYA KANHAR CSE 21103127
+WELCOME to the program .
+ Hope it will help
+Choose the question want to visit: input are set as (1,2,3,4,5,6)!!
+Enter your choice here: 1
+Enter the string:(Leave blank for default) 
+Part A!
+Length os input string 35
+Part B!
+Reverse of string is 'egaugnal evitisnes esac a si nohtyP'
+Part C!
+ a case sensitive
+Part D!
+Python is object oriented language
+Part E
+Index of substring 'a' is 10
+Part F!
+Pythonisacasesensitivelanguage
+You want to try more or exit??
+'Y' for yes, 'N' for no y
+Program by CHAITANYA KANHAR CSE 21103127
+WELCOME to the program again.
+ Hope it will help
+Choose the question want to visit: input are set as (1,2,3,4,5,6)!!
+Enter your choice here: 2
+Please enter your name: Chaitanya
+Enter SID: 21103127
+Enter department name: cse
+Enter CGPA: 10
+
+    
+Hey, Chaitanya Here!
+             My SID is 21103127
+             I am from cse department and my CGPA is 10.0
+You want to try more or exit??
+'Y' for yes, 'N' for no y
+Program by CHAITANYA KANHAR CSE 21103127
+WELCOME to the program again.
+ Hope it will help
+Choose the question want to visit: input are set as (1,2,3,4,5,6)!!
+Enter your choice here: 3
+Part A!
+a&b:  8
+ 
+Part B!
+a|b:  58
+ 
+Part C!
+a^b:  50
+ 
+Part D!
+a<<2: 224 and b<<2:  40
+ 
+Part E!
+a>>2: 14 and b>>4 0
+You want to try more or exit??
+'Y' for yes, 'N' for no y
+Program by CHAITANYA KANHAR CSE 21103127
+WELCOME to the program again.
+ Hope it will help
+Choose the question want to visit: input are set as (1,2,3,4,5,6)!!
+Enter your choice here: 4
+Enter number 1: 12
+Enter number 2: 11
+Enter number 3: 33
+The greatest value is: 33.0
+You want to try more or exit??
+'Y' for yes, 'N' for no y
+Program by CHAITANYA KANHAR CSE 21103127
+WELCOME to the program again.
+ Hope it will help
+Choose the question want to visit: input are set as (1,2,3,4,5,6)!!
+Enter your choice here: 5
+Enter the string: hello guys whats your name
+yes
+You want to try more or exit??
+'Y' for yes, 'N' for no y
+Program by CHAITANYA KANHAR CSE 21103127
+WELCOME to the program again.
+ Hope it will help
+Choose the question want to visit: input are set as (1,2,3,4,5,6)!!
+Enter your choice here: 6
+Enter side 1: 12
+Enter side 2: 33
+Enter side 3: 19
+Input 1 for Question paper's logic
+        Input 2 for myy simple logic!
+Choose the method you want to use:1
+Question paper's method!!
+No
+You want to try more or exit??
+'Y' for yes, 'N' for no n
+Program exited
+Thanks for using!!
